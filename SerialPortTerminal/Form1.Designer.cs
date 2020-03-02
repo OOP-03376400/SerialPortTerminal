590a0171b7d108ce2b904a1cd4b4cd3891be701f
Koson Trachu <koson.trachu@gmail.com>
2020-03-03 01:21:36 +0700

    update

--- SerialPortTerminal/Form1.Designer.cs
+++ SerialPortTerminal/Form1.Designer.cs
@@ -75,13 +75,13 @@
             this.groupBoxSerial.Controls.Add(this.label2);
             this.groupBoxSerial.Controls.Add(this.comboBoxSerialPort);
             this.groupBoxSerial.Controls.Add(this.label1);
-            this.groupBoxSerial.Font = new System.Drawing.Font("微软雅黑 Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBoxSerial.Font = new System.Drawing.Font("Microsoft YaHei Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.groupBoxSerial.Location = new System.Drawing.Point(13, 17);
             this.groupBoxSerial.Name = "groupBoxSerial";
-            this.groupBoxSerial.Size = new System.Drawing.Size(290, 195);
+            this.groupBoxSerial.Size = new System.Drawing.Size(377, 195);
             this.groupBoxSerial.TabIndex = 0;
             this.groupBoxSerial.TabStop = false;
-            this.groupBoxSerial.Text = "串口设置";
+            this.groupBoxSerial.Text = "Serial port setting";
             // 
             // comboBoxStopBits
             // 
@@ -95,7 +95,7 @@
             this.comboBoxStopBits.Location = new System.Drawing.Point(64, 160);
             this.comboBoxStopBits.Margin = new System.Windows.Forms.Padding(2);
             this.comboBoxStopBits.Name = "comboBoxStopBits";
-            this.comboBoxStopBits.Size = new System.Drawing.Size(207, 28);
+            this.comboBoxStopBits.Size = new System.Drawing.Size(308, 25);
             this.comboBoxStopBits.TabIndex = 7;
             this.comboBoxStopBits.SelectedIndexChanged += new System.EventHandler(this.comboBoxStopBits_SelectedIndexChanged);
             // 
@@ -112,7 +112,7 @@
             this.comboBoxParity.Location = new System.Drawing.Point(64, 128);
             this.comboBoxParity.Margin = new System.Windows.Forms.Padding(2);
             this.comboBoxParity.Name = "comboBoxParity";
-            this.comboBoxParity.Size = new System.Drawing.Size(207, 28);
+            this.comboBoxParity.Size = new System.Drawing.Size(308, 25);
             this.comboBoxParity.TabIndex = 7;
             this.comboBoxParity.SelectedIndexChanged += new System.EventHandler(this.comboBoxParity_SelectedIndexChanged);
             // 
@@ -122,9 +122,9 @@
             this.label5.Location = new System.Drawing.Point(6, 162);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.Size = new System.Drawing.Size(51, 17);
             this.label5.TabIndex = 5;
-            this.label5.Text = "停止位";
+            this.label5.Text = "Stop bit";
             // 
             // label4
             // 
@@ -132,9 +132,9 @@
             this.label4.Location = new System.Drawing.Point(6, 131);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.Size = new System.Drawing.Size(38, 17);
             this.label4.TabIndex = 4;
-            this.label4.Text = "校验位";
+            this.label4.Text = "Parity";
             // 
             // comboBoxDataBits
             // 
@@ -146,7 +146,7 @@
             "8"});
             this.comboBoxDataBits.Location = new System.Drawing.Point(64, 93);
             this.comboBoxDataBits.Name = "comboBoxDataBits";
-            this.comboBoxDataBits.Size = new System.Drawing.Size(207, 28);
+            this.comboBoxDataBits.Size = new System.Drawing.Size(308, 25);
             this.comboBoxDataBits.TabIndex = 3;
             this.comboBoxDataBits.Text = "8";
             this.comboBoxDataBits.SelectedIndexChanged += new System.EventHandler(this.comboBoxDataBits_SelectedIndexChanged);
@@ -156,9 +156,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(5, 96);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.Size = new System.Drawing.Size(51, 17);
             this.label3.TabIndex = 2;
-            this.label3.Text = "数据位";
+            this.label3.Text = "Data bit";
             // 
             // comboBoxBaudRate
             // 
@@ -171,7 +171,7 @@
             "115200"});
             this.comboBoxBaudRate.Location = new System.Drawing.Point(64, 60);
             this.comboBoxBaudRate.Name = "comboBoxBaudRate";
-            this.comboBoxBaudRate.Size = new System.Drawing.Size(207, 28);
+            this.comboBoxBaudRate.Size = new System.Drawing.Size(308, 25);
             this.comboBoxBaudRate.TabIndex = 3;
             this.comboBoxBaudRate.Text = "9600";
             this.comboBoxBaudRate.SelectedIndexChanged += new System.EventHandler(this.comboBoxBaudRate_SelectedIndexChanged);
@@ -181,9 +181,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(6, 62);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.Size = new System.Drawing.Size(35, 17);
             this.label2.TabIndex = 2;
-            this.label2.Text = "波特率";
+            this.label2.Text = "Baud";
             // 
             // comboBoxSerialPort
             // 
@@ -192,7 +192,7 @@
             this.comboBoxSerialPort.Location = new System.Drawing.Point(64, 25);
             this.comboBoxSerialPort.MaxDropDownItems = 50;
             this.comboBoxSerialPort.Name = "comboBoxSerialPort";
-            this.comboBoxSerialPort.Size = new System.Drawing.Size(207, 28);
+            this.comboBoxSerialPort.Size = new System.Drawing.Size(308, 25);
             this.comboBoxSerialPort.TabIndex = 1;
             this.comboBoxSerialPort.SelectedIndexChanged += new System.EventHandler(this.comboBoxSerialPort_SelectedIndexChanged);
             // 
@@ -201,29 +201,29 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(14, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.Size = new System.Drawing.Size(31, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "端口";
+            this.label1.Text = "Port";
             // 
             // textBoxSender
             // 
-            this.textBoxSender.Location = new System.Drawing.Point(316, 283);
+            this.textBoxSender.Location = new System.Drawing.Point(396, 278);
             this.textBoxSender.Multiline = true;
             this.textBoxSender.Name = "textBoxSender";
             this.textBoxSender.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxSender.Size = new System.Drawing.Size(266, 85);
+            this.textBoxSender.Size = new System.Drawing.Size(400, 141);
             this.textBoxSender.TabIndex = 2;
             // 
             // textBoxReceiver
             // 
             this.textBoxReceiver.CausesValidation = false;
             this.textBoxReceiver.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.textBoxReceiver.Location = new System.Drawing.Point(316, 17);
+            this.textBoxReceiver.Location = new System.Drawing.Point(396, 12);
             this.textBoxReceiver.Multiline = true;
             this.textBoxReceiver.Name = "textBoxReceiver";
             this.textBoxReceiver.ReadOnly = true;
             this.textBoxReceiver.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxReceiver.Size = new System.Drawing.Size(382, 244);
+            this.textBoxReceiver.Size = new System.Drawing.Size(502, 244);
             this.textBoxReceiver.TabIndex = 1;
             this.textBoxReceiver.TextChanged += new System.EventHandler(this.textBoxReceiver_TextChanged);
             // 
@@ -233,9 +233,9 @@
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabelSerialPortName,
             this.toolStripStatusLabelSerialPortStatus});
-            this.statusStrip.Location = new System.Drawing.Point(0, 382);
+            this.statusStrip.Location = new System.Drawing.Point(0, 430);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(706, 25);
+            this.statusStrip.Size = new System.Drawing.Size(910, 22);
             this.statusStrip.TabIndex = 3;
             this.statusStrip.Text = "statusStrip1";
             // 
@@ -243,7 +243,7 @@
             // 
             this.toolStripStatusLabelSerialPortName.ForeColor = System.Drawing.Color.Blue;
             this.toolStripStatusLabelSerialPortName.Name = "toolStripStatusLabelSerialPortName";
-            this.toolStripStatusLabelSerialPortName.Size = new System.Drawing.Size(142, 20);
+            this.toolStripStatusLabelSerialPortName.Size = new System.Drawing.Size(100, 17);
             this.toolStripStatusLabelSerialPortName.Text = "No COM selected";
             // 
             // toolStripStatusLabelSerialPortStatus
@@ -251,17 +251,17 @@
             this.toolStripStatusLabelSerialPortStatus.ForeColor = System.Drawing.Color.Red;
             this.toolStripStatusLabelSerialPortStatus.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.toolStripStatusLabelSerialPortStatus.Name = "toolStripStatusLabelSerialPortStatus";
-            this.toolStripStatusLabelSerialPortStatus.Size = new System.Drawing.Size(76, 20);
+            this.toolStripStatusLabelSerialPortStatus.Size = new System.Drawing.Size(50, 17);
             this.toolStripStatusLabelSerialPortStatus.Text = "CLOSED";
             // 
             // buttonSend
             // 
-            this.buttonSend.Location = new System.Drawing.Point(608, 335);
+            this.buttonSend.Location = new System.Drawing.Point(819, 371);
             this.buttonSend.Margin = new System.Windows.Forms.Padding(2);
             this.buttonSend.Name = "buttonSend";
             this.buttonSend.Size = new System.Drawing.Size(67, 32);
             this.buttonSend.TabIndex = 4;
-            this.buttonSend.Text = "打开";
+            this.buttonSend.Text = "turn on";
             this.buttonSend.UseVisualStyleBackColor = true;
             this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
             // 
@@ -270,14 +270,14 @@
             this.groupBox1.Controls.Add(this.checkBoxCloseWhenBlur);
             this.groupBox1.Controls.Add(this.radioButtonClose);
             this.groupBox1.Controls.Add(this.radioButtonOpen);
-            this.groupBox1.Location = new System.Drawing.Point(13, 252);
+            this.groupBox1.Location = new System.Drawing.Point(169, 278);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(182, 115);
+            this.groupBox1.Size = new System.Drawing.Size(216, 115);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "串口开关";
+            this.groupBox1.Text = "Switch";
             // 
             // checkBoxCloseWhenBlur
             // 
@@ -285,9 +285,9 @@
             this.checkBoxCloseWhenBlur.Location = new System.Drawing.Point(10, 78);
             this.checkBoxCloseWhenBlur.Margin = new System.Windows.Forms.Padding(2);
             this.checkBoxCloseWhenBlur.Name = "checkBoxCloseWhenBlur";
-            this.checkBoxCloseWhenBlur.Size = new System.Drawing.Size(181, 24);
+            this.checkBoxCloseWhenBlur.Size = new System.Drawing.Size(205, 21);
             this.checkBoxCloseWhenBlur.TabIndex = 1;
-            this.checkBoxCloseWhenBlur.Text = "当窗口失去焦点时关闭";
+            this.checkBoxCloseWhenBlur.Text = "Close when window loses focus";
             this.checkBoxCloseWhenBlur.UseVisualStyleBackColor = true;
             this.checkBoxCloseWhenBlur.CheckedChanged += new System.EventHandler(this.checkBoxCloseWhenBlur_CheckedChanged);
             // 
@@ -299,7 +299,7 @@
             this.radioButtonClose.Location = new System.Drawing.Point(64, 37);
             this.radioButtonClose.Margin = new System.Windows.Forms.Padding(2);
             this.radioButtonClose.Name = "radioButtonClose";
-            this.radioButtonClose.Size = new System.Drawing.Size(39, 30);
+            this.radioButtonClose.Size = new System.Drawing.Size(36, 27);
             this.radioButtonClose.TabIndex = 0;
             this.radioButtonClose.TabStop = true;
             this.radioButtonClose.Text = "Off";
@@ -313,7 +313,7 @@
             this.radioButtonOpen.Location = new System.Drawing.Point(10, 37);
             this.radioButtonOpen.Margin = new System.Windows.Forms.Padding(2);
             this.radioButtonOpen.Name = "radioButtonOpen";
-            this.radioButtonOpen.Size = new System.Drawing.Size(40, 30);
+            this.radioButtonOpen.Size = new System.Drawing.Size(35, 27);
             this.radioButtonOpen.TabIndex = 0;
             this.radioButtonOpen.Text = "On";
             this.radioButtonOpen.UseVisualStyleBackColor = true;
@@ -322,29 +322,29 @@
             // checkBoxAutoEnter
             // 
             this.checkBoxAutoEnter.AutoSize = true;
-            this.checkBoxAutoEnter.Location = new System.Drawing.Point(600, 292);
+            this.checkBoxAutoEnter.Location = new System.Drawing.Point(811, 328);
             this.checkBoxAutoEnter.Margin = new System.Windows.Forms.Padding(2);
             this.checkBoxAutoEnter.Name = "checkBoxAutoEnter";
-            this.checkBoxAutoEnter.Size = new System.Drawing.Size(91, 24);
+            this.checkBoxAutoEnter.Size = new System.Drawing.Size(88, 21);
             this.checkBoxAutoEnter.TabIndex = 6;
-            this.checkBoxAutoEnter.Text = "自动换行";
+            this.checkBoxAutoEnter.Text = "Word wrap";
             this.checkBoxAutoEnter.UseVisualStyleBackColor = true;
             // 
             // buttonClear
             // 
-            this.buttonClear.Location = new System.Drawing.Point(234, 230);
+            this.buttonClear.Location = new System.Drawing.Point(335, 225);
             this.buttonClear.Name = "buttonClear";
             this.buttonClear.Size = new System.Drawing.Size(50, 31);
             this.buttonClear.TabIndex = 7;
-            this.buttonClear.Text = "清除";
+            this.buttonClear.Text = "Clear";
             this.buttonClear.UseVisualStyleBackColor = true;
             this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(706, 407);
+            this.ClientSize = new System.Drawing.Size(910, 452);
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.checkBoxAutoEnter);
             this.Controls.Add(this.groupBox1);
@@ -353,7 +353,7 @@
             this.Controls.Add(this.textBoxSender);
             this.Controls.Add(this.textBoxReceiver);
             this.Controls.Add(this.groupBoxSerial);
-            this.Font = new System.Drawing.Font("微软雅黑 Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Font = new System.Drawing.Font("Microsoft YaHei Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
